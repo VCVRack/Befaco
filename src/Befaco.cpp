042090ae3308a02effc0d617832bc4c8c3e833ac
Andrew Belt <andrewpbelt@gmail.com>
2017-11-11 01:58:32 -0500

    Update createModel()

--- src/Befaco.cpp
+++ src/Befaco.cpp
@@ -9,13 +9,14 @@ void init(rack::Plugin *p) {
 #ifdef VERSION
 	p->version = TOSTRING(VERSION);
 #endif
-	p->addModel(createModel<EvenVCOWidget>("Befaco", "Befaco", "EvenVCO", "EvenVCO"));
-	p->addModel(createModel<RampageWidget>("Befaco", "Befaco", "Rampage", "Rampage"));
-	p->addModel(createModel<ABCWidget>("Befaco", "Befaco", "ABC", "A*B+C"));
-	p->addModel(createModel<SpringReverbWidget>("Befaco", "Befaco", "SpringReverb", "Spring Reverb"));
-	p->addModel(createModel<MixerWidget>("Befaco", "Befaco", "Mixer", "Mixer"));
-	p->addModel(createModel<SlewLimiterWidget>("Befaco", "Befaco", "SlewLimiter", "Slew Limiter"));
-	p->addModel(createModel<DualAtenuverterWidget>("Befaco", "Befaco", "DualAtenuverter", "Dual Atenuverter"));
+
+	p->addModel(createModel<EvenVCOWidget>("Befaco", "EvenVCO", "EvenVCO"));
+	p->addModel(createModel<RampageWidget>("Befaco", "Rampage", "Rampage"));
+	p->addModel(createModel<ABCWidget>("Befaco", "ABC", "A*B+C"));
+	p->addModel(createModel<SpringReverbWidget>("Befaco", "SpringReverb", "Spring Reverb"));
+	p->addModel(createModel<MixerWidget>("Befaco", "Mixer", "Mixer"));
+	p->addModel(createModel<SlewLimiterWidget>("Befaco", "SlewLimiter", "Slew Limiter"));
+	p->addModel(createModel<DualAtenuverterWidget>("Befaco", "DualAtenuverter", "Dual Atenuverter"));
 
 	springReverbInit();
 }
