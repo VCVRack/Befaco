793bc687914526f668fac0a9f0c2e49b225e6b3e
Andrew Belt <andrewpbelt@gmail.com>
2017-11-20 17:39:30 -0500

    Add tags

--- src/Befaco.cpp
+++ src/Befaco.cpp
@@ -10,13 +10,13 @@ void init(rack::Plugin *p) {
 	p->version = TOSTRING(VERSION);
 #endif
 
-	p->addModel(createModel<EvenVCOWidget>("Befaco", "EvenVCO", "EvenVCO"));
-	p->addModel(createModel<RampageWidget>("Befaco", "Rampage", "Rampage"));
-	p->addModel(createModel<ABCWidget>("Befaco", "ABC", "A*B+C"));
-	p->addModel(createModel<SpringReverbWidget>("Befaco", "SpringReverb", "Spring Reverb"));
-	p->addModel(createModel<MixerWidget>("Befaco", "Mixer", "Mixer"));
-	p->addModel(createModel<SlewLimiterWidget>("Befaco", "SlewLimiter", "Slew Limiter"));
-	p->addModel(createModel<DualAtenuverterWidget>("Befaco", "DualAtenuverter", "Dual Atenuverter"));
+	p->addModel(createModel<EvenVCOWidget>("Befaco", "EvenVCO", "EvenVCO", OSCILLATOR_TAG));
+	p->addModel(createModel<RampageWidget>("Befaco", "Rampage", "Rampage", FUNCTION_GENERATOR_TAG, LOGIC_TAG, SLEW_LIMITER_TAG, ENVELOPE_FOLLOWER_TAG, DUAL_TAG));
+	p->addModel(createModel<ABCWidget>("Befaco", "ABC", "A*B+C", RING_MODULATOR_TAG, ATTENUATOR_TAG, DUAL_TAG));
+	p->addModel(createModel<SpringReverbWidget>("Befaco", "SpringReverb", "Spring Reverb", REVERB_TAG, DUAL_TAG));
+	p->addModel(createModel<MixerWidget>("Befaco", "Mixer", "Mixer", MIXER_TAG));
+	p->addModel(createModel<SlewLimiterWidget>("Befaco", "SlewLimiter", "Slew Limiter", SLEW_LIMITER_TAG, ENVELOPE_FOLLOWER_TAG));
+	p->addModel(createModel<DualAtenuverterWidget>("Befaco", "DualAtenuverter", "Dual Atenuverter", ATTENUATOR_TAG, DUAL_TAG));
 
 	springReverbInit();
 }
