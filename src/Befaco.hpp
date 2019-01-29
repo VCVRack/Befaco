53a0b2790f78275af6b20d569a179aa66c0c8b37
Andrew Belt <andrewpbelt@gmail.com>
2019-01-29 13:34:55 -0500

    Fix pluginInstance variable

--- src/Befaco.hpp
+++ src/Befaco.hpp
@@ -5,7 +5,7 @@
 using namespace rack;
 
 
-extern Plugin *plugin;
+extern Plugin *pluginInstance;
 
 extern Model *modelEvenVCO;
 extern Model *modelRampage;
@@ -18,7 +18,7 @@ extern Model *modelDualAtenuverter;
 
 struct Knurlie : SVGScrew {
 	Knurlie() {
-		sw->svg = SVG::load(asset::plugin(plugin, "res/Knurlie.svg"));
+		sw->svg = SVG::load(asset::plugin(pluginInstance, "res/Knurlie.svg"));
 		sw->wrap();
 		box.size = sw->box.size;
 	}
